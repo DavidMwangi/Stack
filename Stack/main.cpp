b3584225a6d65b00b121e6a0551fb824d793e486
Biwott Kevin <Benoni@Biwotts-MacBook-Air.local>
2015-05-20 09:05:39 +0300

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Biwott Kevin on 5/20/15.
+//  Copyright (c) 2015 Biwott Kevin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
